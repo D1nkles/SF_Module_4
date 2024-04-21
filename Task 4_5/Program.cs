6487f08f18eca6ccbb9166f5d20f3b5fe4e16f64
Ilia <isinetov@mail.ru>
2024-04-21 23:43:48 +0400

    Задание 4.5.2.

--- Task 4_5/Program.cs
+++ Task 4_5/Program.cs
@@ -1,5 +1,11 @@
 ﻿static void Main(string[] args) 
 {
     (string name, string lastName, string login, int loginLength, bool hasPet, int age, string[] favoriteColors)User;
-
+    
+    Console.Write("Введите имя пользователя: ");
+    User.name = Console.ReadLine();
+    Console.Write("Введите фамилию пользователя: ");
+    User.lastName = Console.ReadLine();
+    Console.Write("Введите логин пользователя: ");
+    User.login = Console.ReadLine();
 }
