247a5abc1ed563aa80c8f0677708f066dd0c0b8b
Ilia <isinetov@mail.ru>
2024-04-21 22:37:51 +0400

    Задание 4.3.17. Сортировка эл-ов двумерного массива внутри одной строки.

---
+++ Task 4_3_17/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+int current = 0;
+
+for (int i = 0; i <= arr.GetUpperBound(0); i++) 
+{
+    for (int k = 0; k <= arr.GetUpperBound(1); k++) 
+    {
+        for (int j = 0; j <= arr.GetUpperBound(1); j++) 
+        {
+            if (arr[i, k] < arr[i, j]) 
+            {
+                current = arr[i, k];
+                arr[i, k] = arr[i, j];
+                arr[i,j] = current;
+            }
+        }
+    }
+}
+
+for (int row = 0; row <= arr.GetUpperBound(0); row++) 
+{
+    for (int column = 0; column <= arr.GetUpperBound(1); column++) 
+    {
+        Console.Write(arr[row, column] + " ");
+    }
+    Console.WriteLine();
+}
