b38214ff66630a95e5951a8ba9b704a34a1fea23
Ilia <isinetov@mail.ru>
2024-04-21 21:59:30 +0400

    Задание 4.3.16.

---
+++ Task 4_3_16/Program.cs
@@ -0,0 +1,14 @@
+﻿int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+int counter = 0;
+
+for (int i = 0; i <= arr.GetUpperBound(0); i++)
+{
+    for (int k = 0; k <= arr.GetUpperBound(1); k++) 
+    {
+        if (arr[i,k] > 0) 
+        {
+            counter++;
+        }
+    }
+}
+Console.WriteLine(counter);
