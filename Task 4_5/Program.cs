0a3280cdf3668a0cbfe9a83bba55dab390768cb2
Ilia <isinetov@mail.ru>
2024-04-22 00:12:11 +0400

    Задание 4.5.5.

--- Task 4_5/Program.cs
+++ Task 4_5/Program.cs
@@ -1,25 +1,40 @@
-﻿static void Main(string[] args) 
+﻿class Program
 {
-    (string name, string lastName, string login, int loginLength, bool hasPet, int age, string[] favoriteColors)User;
-    
-    Console.Write("Введите имя пользователя: ");
-    User.name = Console.ReadLine();
-
-    Console.Write("Введите фамилию пользователя: ");
-    User.lastName = Console.ReadLine();
-
-    Console.Write("Введите логин пользователя: ");
-    User.login = Console.ReadLine();
-    User.loginLength = User.login.Length;
-
-    Console.WriteLine("Есть ли у вас животные? Да или Нет");
-    string result = Console.ReadLine();
-    if (result.ToLower() == "да")
+    static void Main(string[] args)
     {
-        User.hasPet = true;
-    }
-    else 
-    {
-        User.hasPet = false;
+        (string name, string lastName, string login, int loginLength, bool hasPet, int age, string[] favoriteColors) User;
+
+        Console.Write("Введите имя пользователя: ");
+        User.name = Console.ReadLine();
+
+        Console.Write("Введите фамилию пользователя: ");
+        User.lastName = Console.ReadLine();
+
+        Console.Write("Введите логин пользователя: ");
+        User.login = Console.ReadLine();
+        User.loginLength = User.login.Length;
+
+        Console.WriteLine("Есть ли у вас животные? Да или Нет");
+        string result = Console.ReadLine();
+        if (result.ToLower() == "да")
+        {
+            User.hasPet = true;
+        }
+        else
+        {
+            User.hasPet = false;
+        }
+
+        Console.Write("Введите возраст пользователя: ");
+        User.age = int.Parse(Console.ReadLine());
+
+        User.favoriteColors = new string[3];
+        Console.WriteLine("Введите три любимых цвета пользователя:");
+        for (int i = 1; i <= User.favoriteColors.Length; i++)
+        {
+            Console.Write($"{i}. ");
+            User.favoriteColors[i - 1] = Console.ReadLine();
+        }
+
     }
 }
