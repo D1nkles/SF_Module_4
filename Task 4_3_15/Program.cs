de3108498573a8a402e861f6ba4327bf5e16cd71
Ilia <isinetov@mail.ru>
2024-04-21 21:44:47 +0400

    Задание 4.3.15.

---
+++ Task 4_3_15/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = {1, -24, -6, 25, 88, -7, 2, 10};
+int counter = 0;
+
+foreach (var num in arr) 
+{
+    if (num > 0) 
+    {
+        counter++;
+    }
+}
+Console.WriteLine("Кол-во положительных чисел в массиве: " + counter);
