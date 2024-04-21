faa97a44622133dacf9519e42a146c28a7345a08
Ilia <isinetov@mail.ru>
2024-04-21 23:51:13 +0400

    Задание 4.5.4.

--- Task 4_5/Program.cs
+++ Task 4_5/Program.cs
@@ -12,4 +12,14 @@
     User.login = Console.ReadLine();
     User.loginLength = User.login.Length;
 
+    Console.WriteLine("Есть ли у вас животные? Да или Нет");
+    string result = Console.ReadLine();
+    if (result.ToLower() == "да")
+    {
+        User.hasPet = true;
+    }
+    else 
+    {
+        User.hasPet = false;
+    }
 }
