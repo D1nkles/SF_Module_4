22cd4c457ce0d512e6d53a67600c4117c8be2e57
Ilia <isinetov@mail.ru>
2024-04-21 23:23:49 +0400

    Задание 4.4.5.

---
+++ Task 4_4_5/Program.cs
@@ -0,0 +1,11 @@
+﻿(string Name, string Type, double Age, int NameCount) Pet;
+
+Console.Write("Введите имя питомца: ");
+Pet.Name = Console.ReadLine();
+Pet.NameCount = Pet.Name.Length;
+
+Console.Write("Введите вид питомца: ");
+Pet.Type = Console.ReadLine();
+
+Console.Write("Введите возраст питомца: ");
+Pet.Age = double.Parse(Console.ReadLine());
