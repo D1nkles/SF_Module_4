b7c5d7af8677e4736ffc3e1bbe4d569d598afd28
Ilia <isinetov@mail.ru>
2024-04-21 23:01:07 +0400

    Задание 4.4.2.

---
+++ Task 4_4_2/Program.cs
@@ -0,0 +1,10 @@
+﻿(string name, int age) anketa;
+Console.Write("Введите свое имя: ");
+anketa.name = Console.ReadLine();
+Console.Write("Введите ваш возраст: ");
+anketa.age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("==============================================");
+
+Console.WriteLine($"Ваше имя: {anketa.name}");
+Console.WriteLine($"Ваш возраст: {anketa.age}");
